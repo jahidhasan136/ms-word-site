ff41d789fe5220ec5a1ac95df7d9499407f2ce72
jahidhasan136 <jh3495689@gmail.com>
2023-02-14 04:29:24 +0600

    added ms word code

---
+++ word.js
@@ -0,0 +1,47 @@
+document.getElementById('btn-bold').addEventListener('click', function(){
+    document.getElementById('text-field').style.fontWeight = 'bold';
+});
+
+
+document.getElementById('btn-italic').addEventListener('click', function(){
+    document.getElementById('text-field').style.fontStyle = 'italic';
+});
+
+
+document.getElementById('btn-underline').addEventListener('click', function(){
+    document.getElementById('text-field').style.textDecoration = 'underline';
+});
+
+
+document.getElementById('btn-left').addEventListener('click', function(){
+    document.getElementById('text-field').style.textAlign = 'left';
+});
+
+
+document.getElementById('btn-center').addEventListener('click', function(){
+    document.getElementById('text-field').style.textAlign = 'center';
+});
+
+
+document.getElementById('btn-right').addEventListener('click', function(){
+    document.getElementById('text-field').style.textAlign = 'right';
+});
+
+
+document.getElementById('btn-justify').addEventListener('click', function(){
+    document.getElementById('text-field').style.textAlign = 'justify';
+});
+
+
+document.getElementById('btn-justify').addEventListener('click', function(){
+    document.getElementById('text-field').style.textAlign = 'justify';
+});
+
+
+document.getElementById('color').addEventListener('click', function(){
+    const color = document.getElementById('color');
+    document.getElementById('text-field').style.color = color.value;
+})
+
+
+
